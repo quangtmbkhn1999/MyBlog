bb16adcb3d814eb288a9241b9509e8ab976ab9a7
quangtmbkhn <quangtm.bkhn@gmail.com>
2020-08-03 16:06:24 +0700

    add sign up

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'static_page#home'
   get '/help', to:'static_page#help'
   get '/home', to:'static_page#home'
+  get '/signup',to:'users#new'
+  resources :users
 end
