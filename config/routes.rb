9ddaee035a567ce51093980c7d7caaa47c7933dc
quangtmbkhn <quangtm.bkhn@gmail.com>
2020-08-06 00:30:22 +0700

    basic -login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_page#home'
   get '/help', to:'static_page#help'
   get '/home', to:'static_page#home'
   get '/signup',to:'users#new'
+  get    '/login',   to: 'session#new'
+  post   '/login',   to: 'session#create'
+  delete '/logout',  to: 'session#destroy'
   resources :users
 end
